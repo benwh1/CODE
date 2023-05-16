aa6468d3628864e0f0e4bb95287b8ac2e668dba2
Ben Whitmore <32911992+benwh1@users.noreply.github.com>
2023-05-16 05:40:47 +0100

    Fix `Identifier` parser (require at least one character)

--- src/parser/identifier.rs
+++ src/parser/identifier.rs
@@ -1,10 +1,10 @@
-use nom::{bytes::complete::take_while, IResult};
+use nom::{bytes::complete::take_while1, IResult};
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct Identifier(pub(crate) String);
 
 pub fn identifier(input: &str) -> IResult<&str, Identifier> {
-    let (input, name) = take_while(|c: char| c.is_ascii_alphabetic() || c == ' ')(input)?;
+    let (input, name) = take_while1(|c: char| c.is_ascii_alphabetic() || c == ' ')(input)?;
 
     Ok((input, Identifier(name.to_owned())))
 }
