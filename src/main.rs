2b125bdda12e44cd499a3d0d19e8a9ef47838ccc
Ben Whitmore <32911992+benwh1@users.noreply.github.com>
2023-05-16 21:31:50 +0100

    Add a basic interpreter binary

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use std::{fs::File, io::Read};
+
+use code::{interpreter::interpreter::InterpreterState, parser::program::program};
+
+fn main() {
+    let Some(file_name) = std::env::args().nth(1) else { return };
+    let source_code = {
+        let mut string = String::new();
+        File::open(file_name)
+            .expect("File not found")
+            .read_to_string(&mut string)
+            .expect("Failed to read file");
+        string
+    };
+    let program = program(&source_code);
+    let mut interpreter = InterpreterState::new();
+    interpreter.run(&program);
+}
