3035cf53b6164fa88fae9bd0bb86bebf6feb4685
Ben Whitmore <32911992+benwh1@users.noreply.github.com>
2023-05-16 01:50:52 +0100

    Remove `parse_` prefixes of parser functions

--- src/parser/literal.rs
+++ src/parser/literal.rs
@@ -11,12 +11,12 @@ pub struct IntegerLit(i64);
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct StringLit(String);
 
-pub fn parse_integer(input: &str) -> IResult<&str, IntegerLit> {
+pub fn integer(input: &str) -> IResult<&str, IntegerLit> {
     let (input, lit) = take_while(|c: char| c.is_ascii_digit())(input)?;
 
     Ok((input, IntegerLit(lit.parse().unwrap())))
 }
 
-pub fn parse_string(input: &str) -> IResult<&str, StringLit> {
+pub fn string(input: &str) -> IResult<&str, StringLit> {
     Ok((input, StringLit(input.to_owned())))
 }
--- src/parser/variable_name.rs
+++ src/parser/variable_name.rs
@@ -3,7 +3,7 @@ use nom::{bytes::complete::take_while, IResult};
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct VariableName(String);
 
-pub fn parse_variable_name(input: &str) -> IResult<&str, VariableName> {
+pub fn variable_name(input: &str) -> IResult<&str, VariableName> {
     let (input, name) = take_while(|c: char| c.is_ascii_alphabetic() || c == ' ')(input)?;
 
     Ok((input, VariableName(name.to_owned())))
